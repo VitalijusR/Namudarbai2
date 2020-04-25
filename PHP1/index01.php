353c9cfe1539a8fa6dc6cdb36e9e0e1fa31c59b2
VitalijusR <62949146+VitalijusR@users.noreply.github.com>
2020-04-25 11:13:36 +0300

    all

---
+++ PHP1/index01.php
@@ -0,0 +1,20 @@
+<?php $salis="Lietuva"; ?>
+<?php $miestas="Vilnius"; ?>
+<?php $populiacija="574221"; ?>
+<!DOCTYPE html>
+<html lang="lt">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP1</title>
+</head>
+
+<body>
+    <ul>
+        <li><?php echo $salis; ?></li>
+        <li><?php echo $miestas; ?></li>
+        <li><?php echo $populiacija; ?></li>
+    </ul>
+
+</body>
+</html>
