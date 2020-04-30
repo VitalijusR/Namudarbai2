d2631aec3471c8a08703ae98d83e6d3d27f314bf
VitalijusR <62949146+VitalijusR@users.noreply.github.com>
2020-04-30 15:55:46 +0300

    10

---
+++ PHP10/index10.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP10</title>
+</head>
+
+<body>
+    <?PHP 
+    function daugyba($a, $b) {
+        $suma = $a*$b;
+        echo "Staciakampio plotas: $suma";
+    }
+    
+    daugyba(1,3);
+?>
+    
+</body>
+</html>
